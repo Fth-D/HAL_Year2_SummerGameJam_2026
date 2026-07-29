b9dcfbe9ce44680d20c5731cdeb7277934f3fc7b
1mmorta12345 <xuerenhao30@gmail.com>
2026-07-29 14:41:18 +0900

    Qkey

---
+++ Assets/Scripts/Qkey.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class QKey : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Keyboard keyboard = Keyboard.current;
+
+        bool isPressed =
+            keyboard != null &&
+            keyboard.qKey.isPressed;
+
+        animator.SetBool("IsPress", isPressed);
+    }
+}
